236140464b40c3e86108685ae84b641754b61f5f
NguyenLoc0404 <nttloc0404git@gmail.com>
2023-11-02 11:31:08 +0700

    config again

--- logger.py
+++ logger.py
@@ -1,23 +1,11 @@
 import logging
 
-
 class Logger:
-    """Logger class."""
-
     @classmethod
     def get_logger(cls, name: str):
-        """Logger factory function.
-
-        Args:
-            name (str): Name of the logger.
-
-        Returns:
-            Logger: Logger object.
-        """
         logger = logging.getLogger(name=name)
         logger.setLevel(logging.DEBUG)
-        formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
         ch = logging.StreamHandler()
         ch.setFormatter(formatter)
         logger.addHandler(ch)
@@ -29,8 +17,8 @@ class Logger:
 
 if __name__ == "__main__":
     logger = Logger.get_logger(__name__)
-    logger.info("This is a info message")
-    logger.debug("This is a debug message")
-    logger.warning("This is a warning message")
-    logger.error("This is a error message")
-    logger.critical("This is a critical message")
+    logger.info("This is a info")
+    logger.debug("This is a debug")
+    logger.warning("This is a warning")
+    logger.error("This is a error")
+    logger.critical("This is a critical")
